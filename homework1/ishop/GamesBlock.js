e07c51ba57a44e225bb81a0378cff2b54cfe6659
ryntsevich <ryntsevich.oksana@gmail.com>
2020-06-19 18:12:42 +0300

    refactor: use method map

--- homework1/ishop/GamesBlock.js
+++ homework1/ishop/GamesBlock.js
@@ -3,25 +3,21 @@ var GamesBlock = React.createClass({
     displayName: 'GamesBlock',
 
     render: function () {
-        var gamesHTML = [];
-        for (var i = 0; i < this.props.games.length; i++) {
-            var game = this.props.games[i];
-            var gameHTML = React.DOM.div({ key: game.id, className: 'game' },
-                React.DOM.img({ src: game.imgGame }),
-                React.DOM.div({ className: 'nameGame' }, game.nameGame),
-                React.DOM.div({ className: 'properties' },
-                    React.DOM.div({ className: 'properties_price' }, 'Стоимость: ' + game.price + ' руб.'),
-                    React.DOM.div({ className: 'properties_count' }, 'Осталось коробок: ' + game.count)
-                )
-            );
-            gamesHTML.push(gameHTML);
-        }
+        var games = this.props.games,
+            gamesHTML = games.map(function (game) {
+                return React.DOM.div({ key: game.id, className: 'game' },
+                    React.DOM.img({ src: game.imgGame }),
+                    React.DOM.div({ className: 'nameGame' }, game.nameGame),
+                    React.DOM.div({ className: 'properties' },
+                        React.DOM.div({ className: 'properties_price' }, 'Стоимость: ' + game.price + ' руб.'),
+                        React.DOM.div({ className: 'properties_count' }, 'Осталось коробок: ' + game.count)
+                    )
+                );
+            });
 
         return React.DOM.div({ className: 'gamesBlock' },
             React.DOM.div({ className: 'titleShop' }, this.props.nameShop),
             React.DOM.div({ className: 'games' }, gamesHTML)
         );
-
     }
-
 });
